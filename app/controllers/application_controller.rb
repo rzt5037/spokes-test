b22641f76992f6ae22dc090f4b121ca3bb979219
Richard Travis <r.z.travis@gmail.com>
2018-08-10 11:47:45 -0500

    User attributes added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 class ApplicationController < ActionController::Base
+  include Pundit
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(request.referrer || root_path)
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name,:last_name,:fave_color])
+  end
 end
---
+++ db/migrate/20180810162231_add_name_color_user.rb
@@ -0,0 +1,7 @@
+class AddNameColorUser < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :fave_color, :string
+  end
+end
