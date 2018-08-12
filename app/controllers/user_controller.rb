a2720f4f163f39246e86e4ce1e2a9284c88129b8
Richard Travis <r.z.travis@gmail.com>
2018-08-12 11:47:35 -0500

    Updated User auth and gems for production

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,4 +2,30 @@ class UserController < ApplicationController
   def index
     @users = User.all
   end
+  def edit
+    @user = User.find(params[:id])
+    authorize @user
+  end
+  def update
+    @user = User.find(params[:id])
+
+    @user.assign_attributes(user_params)
+
+    authorize @user
+
+    if @user.save
+      flash[:notice] = "User was updated."
+      redirect_to root_path
+    else
+      flash.now[:alert] = "Error saving user. Please try again."
+      render :edit
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :fave_color)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   get 'welcome/index'
-  get 'user/index'
+  resources :user, only: [:index, :edit, :update]
 
-  root 'welcome#index'
+  root 'user#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
